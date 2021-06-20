99a2c225ef10c4953f59654dbf1f7337c8ba642d
omen <omen@Vijay>
2021-06-20 20:46:08 +0530

    First Commit Uploding files

---
+++ TELUSKO 1/src/iterationdemo.java
@@ -0,0 +1,24 @@
+
+public class iterationdemo {
+
+	public static void main(String[] args) 
+	{
+		int i,j;
+		for(i=1;i<=4;i++)
+		{
+			for(j=1;j<=4;j++) {
+				if(i==1 || i==4 || j==1|| j==4){
+					System.out.print("$ ");
+				}
+				else {
+					System.out.print("  ");
+				}
+				
+		
+			}
+			System.out.println();
+		}
+
+	}
+
+}
